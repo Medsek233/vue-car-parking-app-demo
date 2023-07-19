4c9fc2d94174c229f1a4b39fd93d20ac2e03cf1c
Mohamed Essakili <captainmed510@gmail.com>
2023-07-19 12:36:50 +0100

    Parking List and history

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import './assets/main.css'
+import "@/bootstrap.js";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import ValidationError from "@/components/ValidationError.vue";
+import IconSpinner from "@/components/IconSpinner.vue";
 
-const app = createApp(App)
+import "./assets/main.css";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.component("ValidationError", ValidationError);
+app.component("IconSpinner", IconSpinner);
+
+
+app.mount("#app");
